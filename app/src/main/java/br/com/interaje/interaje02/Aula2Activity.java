1ac5e65bec7219292de7ff5350be875ebdf0e5eb
Felipe Costa <fcostax02@gmail.com>
2016-07-12 14:08:41 -0300

    Posicionamento de Botões e Layouts; Atividade com o toast

---
+++ app/src/main/java/br/com/interaje/interaje02/Aula2Activity.java
@@ -0,0 +1,17 @@
+package br.com.interaje.interaje02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Aula2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aula2);
+
+        // Atividade com os Toasts
+
+        // Atividade com os botões
+    }
+}
